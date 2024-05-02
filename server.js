2e80ad0469d392d4069c620d20e86b4de7a9e4fa
mellacid <melisa.bcd@icloud.com>
2024-05-02 05:47:57 +0200

    added comments for clarification

--- server.js
+++ server.js
@@ -1,7 +1,5 @@
 const WebSocket = require("ws");
 
-console.log(__dirname);
-
 const wss = new WebSocket.Server({ port: 3000 });
 
 wss.on("connection", function connection(ws) {
@@ -9,15 +7,13 @@ wss.on("connection", function connection(ws) {
 
   ws.on("message", function incoming(message) {
     console.log("Nachricht empfangen:", message.toString());
-    // Hier kannst du die empfangenen Befehle verarbeiten und entsprechende Aktionen ausführen
-    // Beispiel:
+    // here you can react to the commands from the bot (bot.js)
     if (message.toString() === "medizin") {
       console.log("Command received: Show medicine");
 
-      const imageUrl = "./img/medizin.png"; // Beispiel-URL, ersetze dies mit der tatsächlichen URL deines Bildes
+      const imageUrl = "./img/medizin.png";
 
-      // Sende die Bild-URL über die WebSocket-Verbindung an den Client
-      ws.send(imageUrl);
+      ws.send(imageUrl); // send the image URL to the client (but this doesn't seem to work yet)
     }
   });
 });

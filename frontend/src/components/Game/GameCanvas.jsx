6ab5ecb60b27660b24de5e7946f02c537dd5bf2e
Jan <j.c.scherch@gmail.com>
2024-05-06 15:35:40 +0200

    feature: add canvas

--- frontend/src/components/Game/GameCanvas.jsx
+++ frontend/src/components/Game/GameCanvas.jsx
@@ -1,7 +1,25 @@
-import React from "react";
+import React, { useRef, useEffect } from "react";
 
 const GameCanvas = () => {
-  return <canvas className="game-canvas" width="352" height="198"></canvas>;
+  const canvasRef = useRef(null);
+
+  useEffect(() => {
+    const canvas = canvasRef.current;
+    const ctx = canvas.getContext("2d");
+
+    //test
+    ctx.fillStyle = "red";
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+  }, []);
+
+  return (
+    <canvas
+      ref={canvasRef}
+      className="game-canvas"
+      width="352"
+      height="198"
+    ></canvas>
+  );
 };
 
 export default GameCanvas;

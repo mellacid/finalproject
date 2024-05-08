f141ffe3bad957a4ed1a4a138e5921c479e4b71c
Jan <j.c.scherch@gmail.com>
2024-05-08 11:53:04 +0200

    delete: files

--- frontend/src/components/Game/GameCanvas.jsx
+++ frontend/src/components/Game/GameCanvas.jsx
@@ -1,17 +1,8 @@
-import React, { useRef, useEffect } from "react";
+import React, { useRef } from "react";
 
 const GameCanvas = () => {
   const gameRef = useRef(null);
   const canvasRef = useRef(null);
-
-  useEffect(() => {
-    const canvas = canvasRef.current;
-    const ctx = canvas.getContext("2d");
-
-    //test
-    ctx.fillStyle = "red";
-    ctx.fillRect(0, 0, canvas.width, canvas.height);
-  }, []);
 
   return (
     <div ref={gameRef} className="game-container">

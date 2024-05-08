031c7a0451a27724f3cfd9bdd67d6efb47b6f7b3
Jan <j.c.scherch@gmail.com>
2024-05-08 15:03:38 +0200

    feature: setup Game basics in canvas

--- frontend/src/components/Game/GameCanvas.jsx
+++ frontend/src/components/Game/GameCanvas.jsx
@@ -1,7 +1,77 @@
-import React, { useRef } from "react";
+import React, { useRef, useEffect, useState } from "react";
+import demoForestImage from "../../assets/images/maps/map.png";
+import playerImage from "../../assets/images/sprites/dog.jpg";
+import shadowImage from "../../assets/images/sprites/shadow.png";
 
 const GameCanvas = () => {
   const canvasRef = useRef(null);
+  const [map, setMap] = useState({
+    image: demoForestImage,
+    player: { image: playerImage, position: { x: 4, y: 0 } },
+  });
+  let ctx = null;
+
+  useEffect(() => {
+    init();
+    createCanvasCtx();
+    drawOverworld();
+  }, []);
+
+  // Overworld
+  function createCanvasCtx() {
+    const canvas = canvasRef.current;
+    ctx = canvas.getContext("2d");
+  }
+
+  function drawOverworld() {
+    // draw map
+    const image = new Image();
+    image.onload = () => {
+      ctx.drawImage(image, 0, 0);
+    };
+    image.src = map.image;
+
+    // draw shadow
+    const shadow = new Image();
+    shadow.onload = () => {
+      ctx.drawImage(
+        shadow,
+        0, // left cut
+        0, // top cut
+        32, // width cut
+        32, // height cut
+        map.player.position.x * 16,
+        map.player.position.y * 16 - 16,
+        32, // width draw
+        32 // height draw
+      );
+    };
+    shadow.src = shadowImage;
+
+    // draw player
+
+    const player = new Image();
+
+    player.onload = () => {
+      ctx.drawImage(
+        player,
+        0, // left cut
+        0, // top cut
+        32, // width cut
+        32, // height cut
+        map.player.position.x * 16,
+        map.player.position.y * 16 - 16,
+        32, // width draw
+        32 // height draw
+      );
+    };
+    player.src = map.player.image;
+  }
+
+  // init game
+  function init() {
+    console.log("It's working!");
+  }
 
   return (
     <canvas

9b126b7f0f5bd002b4c3286fb47bfd056192e2ec
Jan <j.c.scherch@gmail.com>
2024-05-14 11:36:50 +0200

    feature: add useStates for hero and map

--- frontend/src/components/Game/object-graphics/Map.jsx
+++ frontend/src/components/Game/object-graphics/Map.jsx
@@ -5,24 +5,35 @@ import heroImage from "../../../assets/images/sprites/dog.jpg";
 
 const Map = () => {
   const canvasRef = useRef(null);
-  const [map, setMap] = useState();
+  const [map, setMap] = useState({
+    imgSrc: forestMapImage,
+  });
+  const [hero, setHero] = useState({
+    imgSrc: heroImage,
+    position: { x: 0, y: 0 },
+  });
 
   useEffect(() => {
     const canvas = canvasRef.current;
     const ctx = canvas.getContext("2d");
-    const image = new Image();
-    image.src = forestMapImage;
-    image.onload = () => {
-      setMap(image);
-    };
-    ctx.drawImage(image, 0, 0);
 
-    const hero = new Image();
-    hero.src = heroImage;
-    hero.onload = () => {
-      ctx.drawImage(hero, 0, 0, 32, 32, 4 * 16, 1 * 16 - 16, 32, 32);
-    };
+    // draw map
+    const mapDraw = new Image();
+    mapDraw.src = map.imgSrc;
+    ctx.drawImage(mapDraw, 10, 0);
+
+    // draw hero
+
+    drawHero(ctx);
   }, []);
+
+  function drawHero(ctx) {
+    const heroDraw = new Image();
+    heroDraw.src = hero.imgSrc;
+    heroDraw.onload = () => {
+      ctx.drawImage(heroDraw, 0, 0, 32, 32, 4 * 16, 1 * 16 - 16, 32, 32);
+    };
+  }
 
   return <canvas ref={canvasRef}></canvas>;
 };

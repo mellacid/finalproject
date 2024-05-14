1d10c45b01dde553007f19968a2b321501f0bf8a
Jan <j.c.scherch@gmail.com>
2024-05-14 15:38:02 +0200

    feature: add key useState

--- frontend/src/components/Game/Game.jsx
+++ frontend/src/components/Game/Game.jsx
@@ -7,28 +7,49 @@ import heroImage from "../../assets/images/sprites/dog.jpg";
 
 const Game = () => {
   const canvasRef = useRef(null);
+  const [key, setKey] = useState();
   const [map, setMap] = useState({
     imgSrc: forestMapImage,
   });
   const [hero, setHero] = useState({
     imgSrc: heroImage,
-    position: { x: 10.5, y: 6 },
+    direction: "down",
+    isWalking: false,
+    position: { x: 9, y: 4.5 },
   });
 
+  const handleKeyDown = (e) => {
+    if (e.key === "ArrowUp") {
+      setKey("up");
+    } else if (e.key === "ArrowDown") {
+      setKey("down");
+    } else if (e.key === "ArrowLeft") {
+      setKey("left");
+    } else if (e.key === "ArrowRight") {
+      setKey("right");
+    }
+  };
+
   useEffect(() => {
+    console.log("key: ", key);
+
     const canvas = canvasRef.current;
     const ctx = canvas.getContext("2d");
 
     // draw map
+    drawMap(ctx);
+
+    // draw hero
+    drawHero(ctx);
+  }, [hero, key]);
+
+  function drawMap(ctx) {
     const mapDraw = new Image();
     mapDraw.src = map.imgSrc;
     mapDraw.onload = () => {
       ctx.drawImage(mapDraw, 0, 0);
-      drawHero(ctx);
     };
-
-    // draw hero
-  }, []);
+  }
 
   function drawHero(ctx) {
     const heroDraw = new Image();
@@ -40,13 +61,15 @@ const Game = () => {
         0,
         32,
         32,
-        hero.position.x * 16,
-        hero.position.y * 16,
+        hero.position.x * 16 - 8,
+        hero.position.y * 16 - 16,
         32,
         32
       );
     };
   }
+
+  window.addEventListener("keydown", handleKeyDown);
 
   return (
     <div className="game-container">

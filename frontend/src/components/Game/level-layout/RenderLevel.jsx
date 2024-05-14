87f92e72583b41636d4c9cedb22adfb171e8849e
Jan <j.c.scherch@gmail.com>
2024-05-14 09:52:51 +0200

    fix: adjusted style

--- frontend/src/components/Game/level-layout/RenderLevel.jsx
+++ frontend/src/components/Game/level-layout/RenderLevel.jsx
@@ -1,13 +1,11 @@
-import styles from "./RenderLevel.module.css";
-
 import Sprite from "../object-graphics/Sprite.jsx";
 
 import React from "react";
 
 const RenderLevel = ({ spriteSheetImage }) => {
   return (
-    <div className={styles.fullScreenContainer}>
-      <div className={styles.gameScreen}>
+    <div className="fullScreenContainer">
+      <div className="gameScreen">
         <Sprite image={spriteSheetImage} frameCoord={"0x0"} />
         <Sprite image={spriteSheetImage} frameCoord={"2x0"} />
         <Sprite image={spriteSheetImage} frameCoord={"0x2"} />

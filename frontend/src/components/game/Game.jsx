966f358358f019dfaef940d01e026b0608a4f726
Jan <j.c.scherch@gmail.com>
2024-05-15 12:00:38 +0200

    feature: adjust camera movement relative to hero

--- frontend/src/components/game/Game.jsx
+++ frontend/src/components/game/Game.jsx
@@ -10,7 +10,6 @@ import demoForest from "./maps/demoForest.js";
 const Game = () => {
   const canvasRef = useRef(null);
   const [key, setKey] = useState("");
-  const animationRef = useRef(null);
 
   const [map, setMap] = useState(demoForest.map);
   const [hero, setHero] = useState(demoForest.hero);
@@ -19,22 +18,30 @@ const Game = () => {
     const canvas = canvasRef.current;
     const ctx = canvas.getContext("2d");
 
+    const cameraPerson = hero.position;
+
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+
     // draw map
-    drawMap(ctx);
+    drawMap(ctx, cameraPerson);
 
     // draw hero
-    drawHero(ctx);
+    drawHero(ctx, cameraPerson);
   }, [key]);
 
-  function drawMap(ctx) {
+  function drawMap(ctx, cameraPerson) {
     const mapDraw = new Image();
     mapDraw.src = map.imgSrc;
     mapDraw.onload = () => {
-      ctx.drawImage(mapDraw, 0, 0);
+      ctx.drawImage(
+        mapDraw,
+        withGrid(9) - cameraPerson.x,
+        withGrid(4.5) - cameraPerson.y
+      );
     };
   }
 
-  function drawHero(ctx) {
+  function drawHero(ctx, cameraPerson) {
     if (!hero) return;
 
     updateAnimation(hero);
@@ -43,36 +50,29 @@ const Game = () => {
     const frameX = animations[hero.animation][hero.animationFrame][0];
     const frameY = animations[hero.animation][hero.animationFrame][1];
 
+    const x = hero.position.x + withGrid(9) - cameraPerson.x;
+    const y = hero.position.y + withGrid(4.5) - cameraPerson.y;
+
     const heroDraw = new Image();
     heroDraw.src = hero.imgSrc;
     heroDraw.onload = () => {
-      ctx.drawImage(
-        heroDraw,
-        frameX * 32,
-        frameY * 32,
-        32,
-        32,
-        withGrid(hero.position.x) - 8,
-        withGrid(hero.position.y) - 16,
-        32,
-        32
-      );
+      ctx.drawImage(heroDraw, frameX * 32, frameY * 32, 32, 32, x, y, 32, 32);
     };
   }
 
   function walk() {
     if (key === "up") {
       hero.animation = "walk-up";
-      hero.position.y -= 0.1;
+      hero.position.y -= 1;
     } else if (key === "down") {
       hero.animation = "walk-down";
-      hero.position.y += 0.1;
+      hero.position.y += 1;
     } else if (key === "left") {
       hero.animation = "walk-left";
-      hero.position.x -= 0.1;
+      hero.position.x -= 1;
     } else if (key === "right") {
       hero.animation = "walk-right";
-      hero.position.x += 0.1;
+      hero.position.x += 1;
     }
   }
 

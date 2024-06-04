0d8dc9b4fcf099162e582c97e6a46aa39cf9aff2
Jan <j.c.scherch@gmail.com>
2024-06-04 11:59:42 +0200

    fix: back to menu button in Game

--- frontend/src/components/gameSlide.jsx
+++ frontend/src/components/gameSlide.jsx
@@ -1,10 +1,16 @@
 import Game from "./game/Game.jsx";
-
+import { Link } from "react-router-dom";
 const GameSlide = () => (
   <div className="game-slide-container">
     <h1>GAME</h1>
     <Game />
-    <button>Back to menu</button>
+    <Link
+      className="underline"
+      to="/"
+      style={{ textDecoration: "none", color: "black" }}
+    >
+      <button>Back to menu</button>
+    </Link>
   </div>
 );
 

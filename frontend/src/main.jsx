fd02af753a546eeb13b4b87341adaf0f314537a3
Jan <j.c.scherch@gmail.com>
2024-06-04 09:32:04 +0200

    change BrowserRouter to HashRouter

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,15 +2,15 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./components/App.jsx";
 import "./styles/index.css";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import LanguageContextProvider from "./context/LanguageContext.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter>
-    <LanguageContextProvider>
-      <App />
-    </LanguageContextProvider>
-    </BrowserRouter>
+    <HashRouter>
+      <LanguageContextProvider>
+        <App />
+      </LanguageContextProvider>
+    </HashRouter>
   </React.StrictMode>
 );

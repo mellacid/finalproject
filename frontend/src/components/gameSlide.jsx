325671e99337bebd607dba5d3b1db4900022bf5f
Jan <j.c.scherch@gmail.com>
2024-06-04 16:31:36 +0200

    fix: backtomenu button in Game

--- frontend/src/components/gameSlide.jsx
+++ frontend/src/components/gameSlide.jsx
@@ -1,17 +1,22 @@
 import Game from "./game/Game.jsx";
 import { Link } from "react-router-dom";
-const GameSlide = () => (
-  <div className="game-slide-container">
-    <h1>GAME</h1>
-    <Game />
-    <Link
-      className="underline"
-      to="/"
-      style={{ textDecoration: "none", color: "black" }}
-    >
-      <button>Back to menu</button>
-    </Link>
-  </div>
-);
+import { useLanguageContext } from "../context/LanguageContext.jsx";
+import { getTranslation } from "../translations/translations.jsx";
+const GameSlide = () => {
+  const { language } = useLanguageContext();
+  const t = getTranslation(language);
+  return (
+    <div className="game-slide-container">
+      <Game />
+      <Link
+        className="underline"
+        to="/"
+        style={{ textDecoration: "none", color: "black" }}
+      >
+        <button>{t.backtomenu}</button>
+      </Link>
+    </div>
+  );
+};
 
 export default GameSlide;

03f84e886066f4395b83900a164e5f29529d1215
Jan <j.c.scherch@gmail.com>
2024-06-10 13:35:59 +0200

    fix: avoid handleAudio on Keypress

--- frontend/src/components/AudioPlayer.jsx
+++ frontend/src/components/AudioPlayer.jsx
@@ -1,39 +1,47 @@
 //AudioPlayer.jsx
 
-import {useState,useRef } from 'react';
-import src from '../assets/audio/EnchantedShadows.mp3';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faVolumeXmark } from '@fortawesome/free-solid-svg-icons';
-import { faVolumeHigh } from '@fortawesome/free-solid-svg-icons';
+import { useState, useRef } from "react";
+import src from "../assets/audio/EnchantedShadows.mp3";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faVolumeXmark } from "@fortawesome/free-solid-svg-icons";
+import { faVolumeHigh } from "@fortawesome/free-solid-svg-icons";
 
 const AudioPlayer = () => {
   const audioRef = useRef(null);
-const [isPlaying, setIsPlaying] = useState(false);
-
+  const [isPlaying, setIsPlaying] = useState(false);
 
   const handleAudio = () => {
     setIsPlaying(!isPlaying);
     const audio = audioRef.current;
-    audio.loop = true; 
+    audio.loop = true;
     audio.volume = 0.2;
-    
-    if(!isPlaying){
+
+    if (!isPlaying) {
       audio.play();
-      
     } else {
-        audio.pause();
-        }
-
+      audio.pause();
     }
+  };
 
-   return(
+  const handleKeyDown = (event) => {
+    if (event.key === " " || event.key === "Enter") {
+      event.preventDefault();
+    }
+  };
+
+  return (
     <div>
-     <audio ref={audioRef} src={src} />
+      <audio ref={audioRef} src={src} />
 
-    <button id='audio-button'  onClick={handleAudio}>{!isPlaying ? <FontAwesomeIcon icon={faVolumeXmark} /> : <FontAwesomeIcon icon={faVolumeHigh} /> }</button>
+      <button id="audio-button" onKeyDown={handleKeyDown} onClick={handleAudio}>
+        {!isPlaying ? (
+          <FontAwesomeIcon icon={faVolumeXmark} />
+        ) : (
+          <FontAwesomeIcon icon={faVolumeHigh} />
+        )}
+      </button>
     </div>
-    )
-  
+  );
 };
 
 export default AudioPlayer;
